805ceae207952c60a1fba6429b6829b70e32298b
vartikus1337 <pasha_klevtsov@vk.com>
2023-09-16 21:48:58 +0300

    ГГ

--- MyClient.py
+++ MyClient.py
@@ -1,16 +1,12 @@
-import discord
+import discord, argparse
 
 from discord import Member, Message, VoiceState, flags
 from discord.ext import voice_recv
-
-from config import TOKEN
 
 
 class MyClient(discord.Client):
     def __init__(self, intents: flags.Intents):
         super().__init__(intents=intents)
-        self.check_speak = False
-        self.voice_client = None
 
     async def on_ready(self):
         print(f'{self.user.name} has connected to Discord!')
@@ -21,20 +17,29 @@ class MyClient(discord.Client):
 
     async def on_message(self, message: Message):
         if message.author == self.user: return
-        if message.content == 'check_speak': await message.channel.send(self.check_speak); return
         await message.channel.send(f'{message.author.name} сказал: {message.content}')
 
-    def checked_speak(self, user, data):
-        self.check_speak = True
+    def checked_speak(self, user, data: voice_recv.VoiceData):
+        data = data.packet.extension_data.get(voice_recv.ExtensionID.audio_power)
+        power = 127 - (int.from_bytes(data) & 127)
+        print('#' * int(power * (79/128)))
 
     async def on_voice_state_update(self, member: Member, before: VoiceState, after: VoiceState):
         if before.channel is None and after.channel is not None and not after.channel.guild.voice_client:
             voice_channel = self.get_channel(after.channel.id)
-            self.voice_client = await voice_channel.connect(cls=voice_recv.VoiceRecvClient)
-            self.voice_client.listen(voice_recv.BasicSink(self.checked_speak))
+            voice_client = await voice_channel.connect(cls=voice_recv.VoiceRecvClient)
+            voice_client.listen(voice_recv.BasicSink(self.checked_speak))
         elif before.channel is not None and after.channel is None and before.channel.guild.voice_client:
-            self.check_speak = False; await member.guild.voice_client.disconnect(); await client.close()
+            await member.guild.voice_client.disconnect(); await self.close()
 
 
-client = MyClient(intents=discord.Intents.all())
-client.run(TOKEN)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--token', help='Token here')
+    args = parser.parse_args()
+    client = MyClient(intents=discord.Intents.all())
+    client.run(args.token)
+
+
+if __name__ == '__main__':
+    main()
